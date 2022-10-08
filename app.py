ca239bd1c9137dba5e5958ba544ea4c70dbaf3c2
Nicolas Kepper <nicolas.kepper@gmail.com>
2022-10-08 15:31:38 +0200

    run every ten minutes

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ def exec():
 
 
 if __name__ == "__main__":
-    schedule.every(30).seconds.do(exec)
+    schedule.every(10).minutes.do(exec)
 
     while True:
         schedule.run_pending()
-        for i in range(29, -1, -1):
+        for i in range(599, -1, -1):
             sys.stdout.write('\r')
             sys.stdout.write(str(i))
             sys.stdout.flush()
